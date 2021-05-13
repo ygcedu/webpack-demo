9bbd9f8e210446765b8e45fae39e5ab0416a9541
ygc <937601471@qq.com>
2021-05-13 23:00:13 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import x from './x.js'
 import png from './assets/1.png'
 console.log(png)
 
-// const div = document.getElementById('app')
-// console.log('div')
+const div = document.getElementById('app')
+console.log('div')
 // console.log(div)
 // div.innerHTML = `
 //   <img src="${png}">
