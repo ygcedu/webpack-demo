139e8486b78ffa4cbdebddeba55b7c4810bcbca4
ygc <937601471@qq.com>
2021-05-13 22:55:58 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import x from './x.js'
 import png from './assets/1.png'
 console.log(png)
 
-const div = document.getElementById('app')
-console.log('div')
-console.log(div)
-div.innerHTML = `
-  <img src="${png}">
-`
+// const div = document.getElementById('app')
+// console.log('div')
+// console.log(div)
+// div.innerHTML = `
+//   <img src="${png}">
+// `
 
 const button = document.createElement('button')
 button.innerText ='懒加载'
